b017bdcc27cbd4907f0f226dbd02fac2f8ef6729
lucaslinux <lucaspierry00@gmail.com>
2026-06-29 20:16:43 -0300

    fix: remove hard redirect from axios interceptor to prevent login loop

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -11,12 +11,7 @@ const api = axios.create({
 
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      window.location.href = '/login'
-    }
-    return Promise.reject(error)
-  }
+  (error) => Promise.reject(error)
 )
 
 export default api
